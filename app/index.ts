3d982e599c2f9e54dd5f8dc828897b8139ac941f
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-01-02 21:42:52 +0100

    added tsconfig

---
+++ app/index.ts
@@ -0,0 +1,10 @@
+const timeout = n => new Promise(res => setTimeout(res, n))
+
+async function addNums(a, b) {
+  await timeout(500)
+  return a + b
+}
+
+(async () => {
+  console.log('result is ' + await addNums(5, 5))
+})()
